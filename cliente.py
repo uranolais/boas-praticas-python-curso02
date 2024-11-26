cba99743bfc91531b71ea4f113547ab11efabaa4
Laís Urano <lais.urano@gmail.com>
2024-11-26 13:01:27 -0300

    video 1.2

---
+++ cliente.py
@@ -0,0 +1,5 @@
+class Cliente:
+    def __init__(self, nome, endereco):
+        self.nome = nome
+        self.endereco = endereco
+        
---
+++ main.py
@@ -0,0 +1,4 @@
+from cliente import Cliente
+
+cliente = Cliente("Laís", "Alura")
+print(f"Cliente: {cliente.nome}, Endereço: {cliente.endereco}")
