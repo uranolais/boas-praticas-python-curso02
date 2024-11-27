17816ddcd63b14575d286661d35b2c8d8834f173
Laís Urano <lais.urano@gmail.com>
2024-11-27 12:19:59 -0300

    video 2.3

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from cliente import Cliente
 from item import Item
 from pedido.pedido_retirada import PedidoRetirada
+from pedido.pedido_delivery import PedidoDelivery
 
 cliente = Cliente("Laís", "Alura")
 item_um = Item("Pizza", 30.0)
 item_dois = Item("Refrigerante", 5.0)
 itens = [item_um, item_dois]
 
+taxa_entrega = 10.0
 pedido_retirada = PedidoRetirada(cliente, itens)
-print(f"Preço do Pedido Retirada: {pedido_retirada.calcular_total():.2f}")
+pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
+print(f"Preço do Pedido Delivery: {pedido_delivery.calcular_total():.2f}")
