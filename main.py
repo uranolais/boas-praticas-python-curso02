c3a0a6a606ea2f82427f78c0965152e39b00e51b
Laís Urano <lais.urano@gmail.com>
2024-11-27 12:59:47 -0300

    video 3.2

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from cliente import Cliente
 from item import Item
 from pedido.pedido_retirada import PedidoRetirada
 from pedido.pedido_delivery import PedidoDelivery
+from pagamento.pagamento_cartao import PagamentoCartao
+from pagamento.pagamento_pix import PagamentoPIX
 
 cliente = Cliente("Laís", "Alura")
 item_um = Item("Pizza", 30.0)
@@ -11,4 +13,8 @@ itens = [item_um, item_dois]
 taxa_entrega = 10.0
 pedido_retirada = PedidoRetirada(cliente, itens)
 pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
-print(f"Preço do Pedido Delivery: {pedido_delivery.calcular_total():.2f}")
+
+valor_pedido = pedido_delivery.calcular_total()
+pagamento_cartao = PagamentoCartao().processar(valor_pedido)
+pagamento_pix = PagamentoPIX()
+pagamento_pix.processar(valor_pedido)
