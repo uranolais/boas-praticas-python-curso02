80076e834227f8d87d36f3d1292a9f1f74ff9b88
Laís Urano <lais.urano@gmail.com>
2024-11-29 11:36:10 -0300

    video 5.3

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from cliente import Cliente
 from item import Item
-from pedido.pedido_retirada import PedidoRetirada
 from pedido.pedido_delivery import PedidoDelivery
-from pagamento.pagamento_cartao import PagamentoCartao
-from pagamento.pagamento_pix import PagamentoPIX
 from pagamento.pagamento_factory import PagamentoFactory
-from notificacao.notificacao_email import NotificacaoEmail
-from notificacao.notificacao_sms import NotificacaoSMS
 from notificacao.notificacao_facade import NotificacaoFacade
+from observador.observador_status import ObservadorStatus
 
 cliente = Cliente("Laís", "Alura")
 item_um = Item("Pizza", 30.0)
@@ -15,21 +11,20 @@ item_dois = Item("Refrigerante", 5.0)
 itens = [item_um, item_dois]
 
 taxa_entrega = 10.0
-pedido_retirada = PedidoRetirada(cliente, itens)
-pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
+pedido = PedidoDelivery(cliente, itens, taxa_entrega)
 
-valor_pedido = pedido_delivery.calcular_total()
-# pagamento_cartao = PagamentoCartao().processar(valor_pedido)
-# pagamento_pix = PagamentoPIX()
-# pagamento_pix.processar(valor_pedido)
+valor_pedido = pedido.calcular_total()
 tipo_pagamento = "pix"
-pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento)
-pagamento.processar(valor_pedido)
+pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento).processar(valor_pedido)
 
-MENSAGEM = "Seu pedido saiu para entrega!"
-# notificacao_email = NotificacaoEmail().enviar_notificacao(cliente, MENSAGEM)
-# notificacao_sms = NotificacaoSMS().enviar_notificacao(cliente, MENSAGEM)
-notificacoes = NotificacaoFacade().enviar_notificacoes(cliente, MENSAGEM)
+MENSAGEM_PAGO = "O pagamento foi confirmado!"
+MENSAGEM_PREPARANDO = "O pedido está sendo preparado!"
+MENSAGEM_ENVIADO = "O pedido saiu para a entrega!"
 
-pedido_delivery.status = "Pedido confirmado!"
-notificacoes = NotificacaoFacade().enviar_notificacoes(cliente, pedido_delivery.status)
+notificacoes = NotificacaoFacade()
+observador = ObservadorStatus(notificacoes)
+pedido.adicionar_observadores(observador)
+
+pedido.status = MENSAGEM_PAGO
+pedido.status = MENSAGEM_PREPARANDO
+pedido.status = MENSAGEM_ENVIADO
--- observador/observador_status.py
+++ observador/observador_status.py
@@ -3,5 +3,5 @@ class ObservadorStatus:
         self.notificacoes = notificacoes
 
     def atualizar(self, pedido):
-        mensagem = f"O pedido foi atualizado, status: {pedido.status}"
+        mensagem = f"O status do pedido foi atualizado para: {pedido.status}"
         self.notificacoes.enviar_notificacoes(pedido.cliente, mensagem)
